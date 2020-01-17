d92513497b11f0f0f0673e5be789193d50770ebd
Unfinishedgod <38911560+Unfinishedgod@users.noreply.github.com>
2020-01-17 17:00:39 +0900

    Update os_system.py

--- os_system.py
+++ os_system.py
@@ -3,5 +3,7 @@ import os
 
 os.system("/usr/bin/Rscript /home/owen/test/test.R")
 
+#for para in cur:
+    # os.system("/usr/bin/Rscript /home/owen/test/test.R" + " " + "para")
 
 # os.system("/usr/bin/Rscript /home/owen/test/test.R" + " " + "para1" + " " + "para2")
